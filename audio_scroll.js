7adbc4e3c169e6526e72ba147c46a001ec35a235
Allen Chum <chumwai@gmail.com>
2018-03-12 19:57:08 +0800

    formula-sheet

--- audio_scroll.js
+++ audio_scroll.js
@@ -6,8 +6,10 @@
         
         var vol = 1;
         if (deep > height*0.75 && vol>0) {
-			vol = 1-(deep-height)*3/height;
+            vol = 1-(deep-height)*3/height;
+            if(vol>0 && vol<1){
             rainDrop.volume = vol;
+            };
         }
 
     })
